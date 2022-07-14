14d5ccfdc5e84c8bef992588e52b64a22855a93f
KritzHeng <mr.kriskanin@gmail.com>
2022-07-14 19:23:01 +0700

    clearDetail

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,21 +10,15 @@ import {
   getProvider,
   getWalletAddress,
 } from "../services/wallet-service";
-import {
-  getNetworkCurrency,
-  getNetworkName,
-  //   getNetworkTokens,
-} from "../constants/network-id";
 import { formatEther, formatUnits, Logger } from "ethers/lib/utils";
-// import { Token } from "../types/token.type";
-// import Metamask from './Metamask';
+
 import axios from "axios";
 import abi_contract from "../ABI_CONTRACT/abi.json";
 
 const Home: NextPage = () => {
   const [address, setAddress] = useState<string | null>(null);
   const [network, setNetwork] = useState<string | null>(null);
-  //   const [balance, setBalance] = useState<string | null>(null);
+
 
   const [nameToken, setNameToken] = useState<string | null>(null);
   const [maxSupply, setMaxSupply] = useState<string | null>(null);
@@ -70,51 +64,7 @@ const Home: NextPage = () => {
     );
     return contract.supply();
   };
-  // const getBalanceOf = async () => {
 
-  //       const contract = new ethers.Contract(addr_contract, abi_contract, getProvider()!);
-  //       return contract.balanceOf();
-  // }
-
-  // const handlerPublicMint = async () =>{
-
-  //   const addr = getWalletAddress();
-  //   setAddress(addr);
-  //   const chainId = await getChainId();
-  //   setNetwork(chainId);
-
-  //   const tokenBalance = await getTokenBalance(addr_contract, addr).then((res) =>
-  //   formatUnits(res, 0)
-  //   )
-  //   console.log(tokenBalance)
-
-  //     const provider = getProvider()!;
-  //     const signer = provider.getSigner();
-  //     if(Number(tokenBalance) + 1 > 2){
-  //       setStatus("you already have 2 Tickets. ");
-
-  //     }
-  //     else{
-
-  //       // const options = {value: ethers.utils.parseEther("0.01")}
-  //       const contract = new ethers.Contract(addr_contract, abi_contract, signer);
-
-  //       const txResponse = await contract.publicMint(1, {value: ethers.utils.parseEther("0.01"), gasPrice: 300000,
-  //       gasLimit: 9000000});
-
-  //       // try{
-  //         await txResponse.wait();
-
-  //         loadAccountData();
-
-  //       //   {
-  //         //     gasPrice: 100,
-  //         //     gasLimit: 9000000
-  //         // }
-
-  //       }
-
-  // }
   const handlerPublicMint = async () => {
     const addr = getWalletAddress();
     setAddress(addr);
@@ -132,7 +82,7 @@ const Home: NextPage = () => {
     if (Number(tokenBalance) + 1 > 2) {
       setStatus("you already have 2 Tickets. ");
     } else {
-      // // const options = {value: ethers.utils.parseEther("0.01")}
+
       const contract = new ethers.Contract(addr_contract, abi_contract, signer);
       const txResponse = await contract
         .publicMint(1, {
@@ -148,56 +98,10 @@ const Home: NextPage = () => {
           ).href = `https://rinkeby.etherscan.io/tx/${res.hash}`;
         });
 
-      // let link = "https://rinkeby.etherscan.io/tx/" + tx.hash;
-      // console.log(link);
-      // console.log(tx.hash);
-      // setStatus(tx.hash);
-      // await tx
-      //   .wait()
-      //   .catch(() => new Promise((r) => {}))
-      //   .then(() => {});
-      // .on('transactionHash', (transactionHash: any) => {
-      //   // Show tx hash
-      //   console.log(transactionHash)
-
-      //   loadAccountData();
-      //   // $("#tx_link").attr("href", link)
-      //   // $("#tx_link").text(link)
-
-      //   // // Clear status
-      //   // $("#status_value").text('-');
-      // })
-      //     // The transactions was mined without issue
-      //     myProcessMinedTransaction(tx, receipt);
-      //   }
-      // //   catch(_e){
-      // //     let e:Error= _e;
-      // //     result = e.message;
-      // // }
-
-      // // catch(e){
-      // //     result = (e as Error).message;
-      // // }
-
-      //   catch (_e) {
-      //     let error = (_e as Error).message;
-      //     if (error.code === Logger.errors.TRANSACTION_REPLACED) {
-      //       if (error.cancelled) {
-      //         // The transaction was replaced  :'(
-      //         // myProcessCancelledTransaction(tx, error.replacement);
-      //       } else {
-      //         // The user used "speed up" or something similar
-      //         // in their client, but we now have the updated info
-      //         // myProcessMinedTransaction(error.replacement, error.receipt);
-      //       }
-      //     }
-      //   }
-      // setStatus("complete")
-      // loadAccountData();
     }
   };
   const loadAccountData = async () => {
-    // try{
+
 
     const addr = getWalletAddress();
     setAddress(addr);
@@ -217,11 +121,7 @@ const Home: NextPage = () => {
     );
     setCurrentSupply(currentSup);
     console.log(currentSup);
-    // }
-    // catch(e){
-    //   console.log("Error getting contract");
-    //   return;
-    // }
+
   };
 
   useEffect(() => {
@@ -244,15 +144,6 @@ const Home: NextPage = () => {
     getEthereum()?.on("chainChanged", handleNetworkChange);
   }, []);
 
-  // function openContractOnEtherScan() {
-  //   let url = "https://" + network + ".etherscan.io/token/" + addr_contract;
-  //   window.open(url, "_blank");
-  // }
-
-  // function openUserAddressOnEtherScan() {
-  //   let url = 'https://' + network + '.etherscan.io/address/' + userAccount
-  //   window.open(url, '_blank');
-  // }
   return (
     <div className="bg-[#002368]">
       <div>
